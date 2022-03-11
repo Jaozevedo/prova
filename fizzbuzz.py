4d2d73f42e93be2a809964d6cf2108c66a1f3a17
Jaozevedo <98059839+Jaozevedo@users.noreply.github.com>
2022-03-10 22:57:40 -0300

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,26 +1,25 @@
 # -*- coding: utf-8 -*-
 """
 Crie um programa que leia um número n e retorne a sequência de Fizz-Buzz até este número.
-
 A sequência Fizz-Buzz é feita imprimindo cada número natural, mas trocando os múltiplos 
 de 3 por "fizz", os múltiplos de 5 por "buzz" e os múltiplos de ambos por "fizzbuzz".
-
 * for: 1
 * if: 1
 """
 
-break
-elif i % 15 == 0:
+n = int(input(("n: ")))
+for i in range(1, n + 1):
+    if n <= 0:
+        print("numero invalido")
     elif i % 3 == 0:
-        elif i % 5 == 0:
-            else:
-                for i in range(1, n + 1):
-                    for i in range(n + 1):
-                        for i in range(n):
-                            if n <= 0:
-                                n = int(input(("n: ")))
-                                print("buzz")
-                                print("fizz")
-                                print("fizzbuzz")
-                                print("numero invalido")
-                                print(i)
+        print("fizz")
+    elif i % 5 == 0:
+        print("buzz")
+    elif i % 15 == 0:
+        print("fizzbuzz")
+    else:
+        print(i)
+
+#for i in range(n + 1):
+#for i in range(n):
+#break
